4a9abe11681395e57491461a612a26b33e0c8a31
kuro-channel <takumikuroiwa@gmail.com>
2022-01-11 07:38:39 +0900

    20220111
    アルゴリズム練習 朝活

--- algorithm_math/chapter2/Practice.java
+++ algorithm_math/chapter2/Practice.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -18,10 +19,21 @@ public class Practice {
 		System.out.println("A1：" + a1);
 		System.out.println("A2：" + a2);
 		System.out.println("A3：" + a3);
-		int[] aArray = {a1, a2, a3};
+		int[] aArray = { a1, a2, a3 };
 		practice2(aArray);
-	}
 
+		// 問題ID: 003
+		int n = rand.nextInt(100);
+		System.out.println("N:" + n);
+		List<Integer> nList = new ArrayList<>();
+
+		for (int i = 0; i < n; i++) {
+			int tmpNum = rand.nextInt(100);
+			nList.add(tmpNum);
+			System.out.println("tmpNum:" + tmpNum);
+		}
+		practice3(n, nList);
+	}
 
 	/**
 	 * p.16 リンゴが5個, みかんがN個
@@ -44,5 +56,22 @@ public class Practice {
 	private static void practice2(int[] aArray) {
 		int sum = Arrays.stream(aArray).sum();
 		System.out.println("002 合計：" + sum + "個");
+	}
+
+	/**
+	 * p.17~18 整数NとN個の整数
+	 * A1+A2・・・AのNを出力
+	 * 
+	 * @param n     整数N
+	 * @param nList N個の整数
+	 */
+	private static void practice3(int n, List<Integer> nList) {
+		int sum = 0;
+
+		// N個の整数分ループ
+		for (Integer num : nList) {
+			sum += num;
+		}
+		System.out.println("003 合計：" + sum);
 	}
 }
